13f5f9dd91f20cf4f86128110d811471d0be32f3
Pavithra Kumar <pavi@digiryte.com>
2020-07-02 17:26:34 +0530

    Basic grid layout

--- lib/main.dart
+++ lib/main.dart
@@ -4,50 +4,56 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count, (i) => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(width: 2, color: Colors.black12),
+          borderRadius: const BorderRadius.all(const Radius.circular(8)),
+        ),
+
+        margin: const EdgeInsets.all(3),
+        child: Image.asset('assets/images/sm$i.png')));
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        appBar: AppBar(
+          centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            leading: Icon(Icons.menu),
+            title: Text('Level 1'),
+            actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'settings',
+              onPressed: () {
+              }
+            )
+          ]
+          ),
+        body: Center(
+          child: _buildGrid(),
+        ),
       ),
     );
   }
+
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 90,
+      padding: const EdgeInsets.all(100.0),
+      mainAxisSpacing: 1,
+      crossAxisSpacing: 1,
+      children: _buildGridTileList(9));
+
+// The images are saved with names pic0.jpg, pic1.jpg...pic29.jpg.
+// The List.generate() constructor allows an easy way to create
+// a list when objects have a predictable naming pattern.
+
 }
+
+
+
+
